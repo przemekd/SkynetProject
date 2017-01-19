f8f7a713b82756e47bf8dcec26955a6abc66fac6
Przemyslaw Dubaniewicz <przemolo@gmail.com>
2017-01-19 00:05:19 +0000

    Small fixes

--- setup/data_ingestion.sh
+++ setup/data_ingestion.sh
@@ -7,7 +7,9 @@ unzip ReleasableAircraft.zip
 
 hdfs dfs -mkdir /data/flightdata/faa
 hdfs dfs -mkdir /data/flightdata/faa/master
+hdfs dfs -mkdir /data/flightdata/faa/dereg
 hdfs dfs -mkdir /data/flightdata/faa/aircraft_references
 
 hdfs dfs -copyFromLocal MASTER.txt  /data/flightdata/faa/master
 hdfs dfs -copyFromLocal ACFTREF.txt /data/flightdata/faa/aircraft_references
+hdfs dfs -copyFromLocal DEREG.txt /data/flightdata/faa/dereg
